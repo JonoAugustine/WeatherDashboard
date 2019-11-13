8b223328a38794bd6a2da36197c5f7ef769cab8c
jonoaugustine <jonoaugustine@jhu.edu>
2019-11-13 18:17:24 -0500

    ajax appending url to local adress??

--- assets/js/dao.js
+++ assets/js/dao.js
@@ -3,14 +3,18 @@ const weatherApi = {
   uri: "api.openweathermap.org/data/2.5"
 };
 
-const queryUrl = (type = `${weatherApi.uri}/${type}?appid=${weatherApi.key}`);
+const queryUrl = type => `${weatherApi.uri}/${type}?appid=${weatherApi.key}`;
 
 const query = (type, queryString) =>
   $.ajax({
     type: "GET",
-    url: `${queryUrl(type)}&${queryString}`
+    url: `${queryUrl(type)}&${queryString}`,
+    crossDomain: true
   });
 
-const weather = (city, country) => {};
+const weather = (city, country) =>
+  query("weather", `q=${city}${country ? "," + country : ""}`);
 
 const forecast = (city, country) => {};
+
+weather("rockville", "us").then(r => console.log(r));
